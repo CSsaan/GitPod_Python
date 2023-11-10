005d04e6b4294df0821209050488d38542a98732
CSsaan <1179593516@qq.com>
2023-11-10 08:52:07 +0000

    拟合曲线保存表格

--- Nonlinear_fitting.py
+++ Nonlinear_fitting.py
@@ -3,80 +3,130 @@
 import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
+import pandas as pd
 
-
+a, b, c = 0, 0, 0
 ########################################################################################
 # 拟合曲线
 ########################################################################################
-# USE_BAIDU = 1
+if 1:
+    USE_BAIDU = 1
+    # 给定一组非线性数据
+    xdata = np.array([0.35, 18.47, 57.89])
+    ydata = np.array([0.01, 0.94, 0.967])
 
-# # 给定一组非线性数据
-# xdata = np.array([0.53, 0.61, 0.69, 0.76, 0.84, 0.91, 1.07, 1.22, 1.37, 1.52,
-#                  1.68, 1.83, 2.13, 2.44, 3.05, 3.66, 4.57, 6.1, 9.14, 18.2])
-# ydata = np.array([0.0, 0.057029096, 0.210707504, 0.301353752, 0.388998469, 
-#                  0.453831547, 0.55648392, 0.621917305, 0.672343032, 0.712563553,
-#                  0.744980092,0.769592649, 0.806811639, 0.834725881, 0.871344564,
-#                  0.894756508, 0.917267994, 0.939179173, 0.95988974, 0.98])
+    xfitdata = np.array(
+        [
+            0.53,
+            0.58,
+            0.61,
+            0.65,
+            0.69,
+            0.7,
+            0.76,
+            0.8,
+            0.84,
+            0.87,
+            0.91,
+            1.0,
+            1.07,
+            1.15,
+            1.22,
+            1.3,
+            1.37,
+            1.4,
+            1.52,
+            1.6,
+            1.68,
+            1.7,
+            1.83,
+            2.0,
+            2.13,
+            2.2,
+            2.44,
+            2.8,
+            3.05,
+            3.3,
+            3.66,
+            4.1,
+            4.57,
+            4.9,
+            6.1,
+            8.1,
+            9.14,
+            12.0,
+            15.0,
+            18.2,
+        ]
+    )
 
-# xfitdata = np.array([0.53,0.58, 0.61,0.65, 0.69, 0.7,0.76,0.8, 0.84,0.87, 0.91,1.0, 1.07,1.15, 1.22,1.3, 1.37,1.4, 1.52,1.6,
-#                  1.68,1.7, 1.83,2.0, 2.13,2.2, 2.44,2.8, 3.05,3.3, 3.66,4.1, 4.57,4.9, 6.1,8.1, 9.14,12.0,15.0, 18.2])
+    # xdata = np.array([1, 2, 3, 4, 5])
+    # ydata = np.array([0.5, 0.8, 1.2, 1.6, 2.0])
 
-# # xdata = np.array([1, 2, 3, 4, 5])
-# # ydata = np.array([0.5, 0.8, 1.2, 1.6, 2.0])
+    if USE_BAIDU:
+        # (1)定义对数函数模型
+        def log_func(x, a, b):
+            return a + b * np.log(x)
 
-# if(USE_BAIDU):
-#     # (1)定义对数函数模型
-#     def log_func(x, a, b):
-#         return a + b * np.log(x)
-#     # (2)定义多项式函数模型
-#     def multix_func(x, a, b, c, d, e, f):
-#         return a*x + b*x**2 + c*x**3 + d*x**4 + e*x**5 + f
-#     # (3)定义4参数方程拟合
-#     def params4_func(x, a, b, c, d):
-#         return (a - d) / (1 + (x / c) ** b) + d
-#     # 使用最小二乘法等优化算法，求解出参数 a 和 b 的最优解
-#     # popt, pcov = curve_fit(log_func, xdata, ydata)
-#     # popt, pcov = curve_fit(multix_func, xdata, ydata)
-#     popt, pcov = curve_fit(params4_func, xdata, ydata)
-#     # 输出拟合参数 a 和 b 的值
-#     # print("a = %.3f, b = %.3f" % (popt[0], popt[1]))
-#     for _iter in popt:
-#         print(_iter)
-#     # 使用求得的参数 a 和 b，得出每个 x 值所对应的 y 值
-#     # yfit = log_func(xdata, *popt) # (1)
-#     # yfit = multix_func(xdata, *popt) # (2)
-#     yfit = params4_func(xfitdata, *popt) # (3)
-#     # 绘制拟合曲线和原始数据
-#     plt.plot(xfitdata, yfit, 'r-', label='log fit')
-#     plt.plot(xdata, ydata, 'bo', label='original data')
-#     plt.legend()
-#     plt.savefig('output/log_plot_baidu.jpg')
+        # (2)定义多项式函数模型
+        def multix_func(x, a, b, c, d, e, f):
+            return a * x + b * x**2 + c * x**3 + d * x**4 + e * x**5 + f
 
-# else:
-#     # 定义对数回归函数形式
-#     def logarithmic_func(x, a, b):
-#         return a * np.log(x) + b
-#     # # 创建示例数据
-#     # x = np.array([1, 2, 3, 4, 5, 6])
-#     # y = np.array([1.2, 2.5, 3.6, 4.0, 4.2, 4.3]) # 1.2, 2.5, 3.6, 4.0, 4.2, 4.3        100, 200, 300, 400, 500, 600
-#     # 使用curve_fit函数进行对数回归拟合
-#     params, covariance = curve_fit(logarithmic_func, xdata, ydata)
-#     # 从参数中获取拟合结果
-#     a, b = params
-#     # 打印拟合参数
-#     print("a =", a)
-#     print("b =", b)
-#     # 使用拟合结果绘制拟合曲线
-#     x_fit = np.linspace(1, 6, 100)  # 创建用于拟合曲线的新x值
-#     y_fit = logarithmic_func(x_fit, a, b)
-#     # 绘制原始数据和拟合曲线
-#     plt.scatter(xdata, ydata, label='Data')
-#     plt.plot(x_fit, y_fit, 'r-', label='Logarithmic Fit')
-#     plt.xlabel('X')
-#     plt.ylabel('Y')
-#     plt.legend()
-#     plt.savefig('output/log_plot_gpt.jpg')
-    
+        # (3)定义4参数方程拟合
+        def params4_func(x, a, b, c, d):
+            return (a - d) / (1 + (x / c) ** b) + d
+
+        # (4)二次函数
+        def quadratic_func(x, a, b, c):  # a, b, c = 0.113, -0.830, 0.984
+            x = 1.0 / x
+            return a * x * x + b * x + c
+
+        # 使用最小二乘法等优化算法，求解出参数 a 和 b 的最优解
+        # popt, pcov = curve_fit(log_func, xdata, ydata)
+        # popt, pcov = curve_fit(multix_func, xdata, ydata)
+        #     popt, pcov = curve_fit(params4_func, xdata, ydata)
+        popt, pcov = curve_fit(quadratic_func, xdata, ydata)
+        # 输出拟合参数 a 和 b 的值
+        # print("a = %.3f, b = %.3f" % (popt[0], popt[1]))
+        a, b, c = popt
+        for _iter in popt:
+            print(_iter)
+        # 使用求得的参数 a 和 b，得出每个 x 值所对应的 y 值
+        # yfit = log_func(xdata, *popt) # (1)
+        # yfit = multix_func(xdata, *popt) # (2)
+        #     yfit = params4_func(xfitdata, *popt) # (3)
+        yfit = quadratic_func(xfitdata, *popt)  # (4)
+        # 绘制拟合曲线和原始数据
+        plt.plot(xfitdata, yfit, "r-", label="log fit")
+        plt.plot(xdata, ydata, "bo", label="original data")
+        plt.legend()
+        plt.savefig("output/log_plot_baidu.jpg")
+
+    else:
+        # 定义对数回归函数形式
+        def logarithmic_func(x, a, b):
+            return a * np.log(x) + b
+
+        # # 创建示例数据
+        # x = np.array([1, 2, 3, 4, 5, 6])
+        # y = np.array([1.2, 2.5, 3.6, 4.0, 4.2, 4.3]) # 1.2, 2.5, 3.6, 4.0, 4.2, 4.3        100, 200, 300, 400, 500, 600
+        # 使用curve_fit函数进行对数回归拟合
+        params, covariance = curve_fit(logarithmic_func, xdata, ydata)
+        # 从参数中获取拟合结果
+        a, b = params
+        # 打印拟合参数
+        print("a =", a)
+        print("b =", b)
+        # 使用拟合结果绘制拟合曲线
+        x_fit = np.linspace(1, 6, 100)  # 创建用于拟合曲线的新x值
+        y_fit = logarithmic_func(x_fit, a, b)
+        # 绘制原始数据和拟合曲线
+        plt.scatter(xdata, ydata, label="Data")
+        plt.plot(x_fit, y_fit, "r-", label="Logarithmic Fit")
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.legend()
+        plt.savefig("output/log_plot_gpt.jpg")
 
 
 ########################################################################################
@@ -104,59 +154,146 @@ import matplotlib.pyplot as plt
 # #         print(x_look_point[i], "m: ", y_look_point[i])
 
 
-
-
 # def fan_logarithmic_func(x):
-#         return 2.718**((x-0.247)*4.0) # 2.718281828459045**((x-0.247368521715951)*4.0)   
+#         return 2.718**((x-0.247)*4.0) # 2.718281828459045**((x-0.247368521715951)*4.0)
 #         # 为降低运算，近似泰勒级数展开： 1 + ((x-0.247)*4.0) + (((x-0.247)*4.0)^2 / 2!) + (((x-0.247)*4.0)^3 / 3!) + (((x-0.247)*4.0)^4 / 4!)
 # x_fit = np.linspace(0.01, 1.0, 21)
 # y_fit = fan_logarithmic_func(x_fit)
 # print(x_fit) # [0.01, 0.0595, 0.109, 0.1585, 0.208, 0.2575, 0.307, 0.3565, 0.406, 0.4555, 0.505, 0.5545, 0.604, 0.6535, 0.703, 0.7525, 0.802, 0.8515, 0.901, 0.9505, 1.0]
-# print(y_fit) # [ 0.38694447, 0.47167076, 0.57494891, 0.7008411, 0.85429895, 1.04135829, 1.2693766, 1.54732234, 1.88612774, 2.29911879, 
+# print(y_fit) # [ 0.38694447, 0.47167076, 0.57494891, 0.7008411, 0.85429895, 1.04135829, 1.2693766, 1.54732234, 1.88612774, 2.29911879,
 #              #   2.80253934, 3.41619006, 4.16420722, 5.076012, 6.18746773, 7.54229048, 9.19376846, 11.20685801, 13.66073847, 16.65192646, 20.29807214]
-
 
 
 ########################################################################################
 # draw line(二次函数曲线)
 ########################################################################################
-def quadratic_func(x):  # x:0-18  -> y:0-1
-        x = 1.0/x
-        return 0.113*x*x - 0.830*x + 0.984
+if 0:
+
+    def quadratic_func(x):  # x:0-18  -> y:0-1
+        x = 1.0 / x
+        return 0.113 * x * x + (-0.830 + 0.16) * x + 0.984
         # return (384.36*x*x - 2822.77*x + 3347.42)/3400.0
 
-def AFStep2Dis(step):
+    def AFStep2Dis(step):
+        a, b, c = 0.113, -0.830 + 0.16, 0.984
         rrr = np.zeros(len(step))
         for i in range(len(step)):
-                delta = 0.830**2 - 4*0.113*(0.984 - step[i])
-                if delta >= 0:
-                        z1 = (0.830 + np.sqrt(delta)) / (2.0 * 0.113)
-                        result = 1.0/((0.830 - np.sqrt(delta)) / (2.0 * 0.113))
-                        # if(result<0):
-                        #         result = 0
-                        print(i+1, ":",result)
-                        rrr[i] = result
-                        # print(rrr[i])
-                
+            delta = b**2 - 4 * a * (c - step[i])
+            if delta >= 0:
+                z1 = (-b + np.sqrt(delta)) / (2.0 * a)
+                result = 1.0 / ((-b - np.sqrt(delta)) / (2.0 * a))
+                # if(result<0):
+                #         result = 0
+                print(i + 1, ":", result)
+                rrr[i] = max(result, 0)
+                # print(rrr[i])
         return rrr
-x_look_point = np.linspace(0.0, 1.0, 100) # np.array([0.38694447, 0.47167076, 0.57494891, 0.7008411, 0.85429895, 1.04135829, 1.2693766, 1.54732234, 1.88612774, 2.29911879, 2.80253934, 3.41619006, 4.16420722, 5.076012, 6.18746773, 7.54229048, 9.19376846, 11.20685801, 13.66073847, 16.65192646, 20.29807214]) 
-y_look_point = AFStep2Dis(x_look_point)
-print(y_look_point)
 
-# 绘制原始数据和拟合曲线
-plt.scatter(np.zeros(len(x_look_point)), y_look_point, s=2, c='b', label='look tick')
-plt.scatter(x_look_point, y_look_point, label='look Data')
-plt.plot(x_look_point, y_look_point, 'r-', label='Logarithmic')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.legend()
-plt.savefig('output/log_plot_line.jpg')
+    x_look_point = np.linspace(
+        0.0, 1.0, 100
+    )  # np.array([0.38694447, 0.47167076, 0.57494891, 0.7008411, 0.85429895, 1.04135829, 1.2693766, 1.54732234, 1.88612774, 2.29911879, 2.80253934, 3.41619006, 4.16420722, 5.076012, 6.18746773, 7.54229048, 9.19376846, 11.20685801, 13.66073847, 16.65192646, 20.29807214])
+    y_look_point = AFStep2Dis(
+        x_look_point
+    )  # AFStep2Dis(x_look_point)  # quadratic_func(x_look_point)
+    print(y_look_point)
+
+    # 绘制原始数据和拟合曲线
+    plt.scatter(
+        np.zeros(len(x_look_point)), y_look_point, s=2, c="b", label="look tick"
+    )
+    plt.scatter(x_look_point, y_look_point, label="look Data")
+    plt.plot(x_look_point, y_look_point, "r-", label="Logarithmic")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.legend()
+    plt.savefig("output/log_plot_line.jpg")
+
+    # for i in range(len(x_look_point)):
+    #         print(x_look_point[i], "m: ", y_look_point[i])
+
+    print(
+        y_look_point[0],
+        y_look_point[1],
+        y_look_point[2],
+        y_look_point[3],
+        y_look_point[4],
+    )
+    print(
+        y_look_point[95],
+        y_look_point[96],
+        y_look_point[97],
+        y_look_point[98],
+        y_look_point[99],
+    )
+    # print(0.984 - ((-0.830)**2)/(4*0.113)) # 极值点: c-(b^2)/(4a)
+    # print(quadratic_func(18.08076053706008))
+    # print(quadratic_func(6))
 
 
-# for i in range(len(x_look_point)):
-#         print(x_look_point[i], "m: ", y_look_point[i])
+########################################################################################
+# 保存以上刻度表格
+########################################################################################
+if 1:
+    # a, b, c = 0.113+0.05, -0.830+0.12, 0.984-0.15
+    # a, b, c = 0.141, -0.742, 0.979
 
+    def quadratic_func(x):  # x:0-18  -> y:0-1
+        x = 1.0 / x
+        return a * x * x + b * x + c
+        # return (384.36*x*x - 2822.77*x + 3347.42)/3400.0
 
-print(quadratic_func(1.62167287185891))
-# print(quadratic_func(18.08076053706008))
-# print(quadratic_func(6))
+    def AFStep2Dis(step):
+        rrr = np.zeros(len(step))
+        for i in range(len(step)):
+            delta = b**2 - 4 * a * (c - step[i])
+            if delta >= 0:
+                z1 = (-b + np.sqrt(delta)) / (2.0 * a)
+                result = 1.0 / ((-b - np.sqrt(delta)) / (2.0 * a))
+                # if(result<0):
+                #         result = 0
+                print(100 - i, ":", result)
+                rrr[i] = max(result, 0)
+                # print(rrr[i])
+        return rrr
+
+    x_look_point = np.linspace(
+        1.0, 0.0, 100
+    )  # np.array([0.38694447, 0.47167076, 0.57494891, 0.7008411, 0.85429895, 1.04135829, 1.2693766, 1.54732234, 1.88612774, 2.29911879, 2.80253934, 3.41619006, 4.16420722, 5.076012, 6.18746773, 7.54229048, 9.19376846, 11.20685801, 13.66073847, 16.65192646, 20.29807214])
+    y_look_point = AFStep2Dis(
+        x_look_point
+    )  # AFStep2Dis(x_look_point)  # quadratic_func(x_look_point)
+    print(y_look_point)
+    df = pd.DataFrame({"位置": np.arange(len(y_look_point), 0, -1), "刻度值": y_look_point})
+    df.to_csv("output.csv", index=False)
+
+    # 绘制原始数据和拟合曲线
+    plt.scatter(
+        np.zeros(len(x_look_point)), y_look_point, s=2, c="b", label="look tick"
+    )
+    plt.scatter(x_look_point, y_look_point, label="look Data")
+    plt.plot(x_look_point, y_look_point, "r-", label="Logarithmic")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.legend()
+    plt.savefig("output/log_plot_line.jpg")
+
+    print(
+        y_look_point[0],
+        y_look_point[1],
+        y_look_point[2],
+        y_look_point[3],
+        y_look_point[4],
+    )
+    print(
+        y_look_point[95],
+        y_look_point[96],
+        y_look_point[97],
+        y_look_point[98],
+        y_look_point[99],
+    )
+    print(
+        quadratic_func(0.55),
+        quadratic_func(1.55),
+        quadratic_func(18.55),
+        quadratic_func(27.55),
+    )
--- tickalpha.py
+++ tickalpha.py
@@ -1,28 +1,52 @@
-import  cv2
+# import  cv2
 
-img = cv2.imread('tick.jpg')
-print(img.shape)
+# img = cv2.imread('tick.jpg')
+# print(img.shape)
 
-# 分离通道,最后是alpha
-b, g, r = cv2.split(img)
-w,h = b.shape
+# # 分离通道,最后是alpha
+# b, g, r = cv2.split(img)
+# w,h = b.shape
 
-sum = 0
-for i in range(w):
-    for j in range(h):
+# sum = 0
+# for i in range(w):
+#     for j in range(h):
         
-        if(img[i,j,2] < 240):
-            sum+=1
-            print(img[i,j,2])
-            # img[i,j,3] = 255
-        else:
-            img[i,j,0] = 0
-            img[i,j,1] = 0
-            img[i,j,2] = 0
+#         if(img[i,j,2] < 240):
+#             sum+=1
+#             print(img[i,j,2])
+#             # img[i,j,3] = 255
+#         else:
+#             img[i,j,0] = 0
+#             img[i,j,1] = 0
+#             img[i,j,2] = 0
 
             
-print(sum/(w*h))
+# print(sum/(w*h))
 
-cv2.imwrite("new.jpg", img)
+# cv2.imwrite("new.jpg", img)
     
     
+import difflib
+
+file1 = r'E:\test\aaaa\aaa\aa.txt'
+file2 = r'E:\test\aaaa\aaa\bb.txt'
+
+with open(file1, 'rb') as f1, open(file2, 'rb') as f2:
+    content1 = f1.read()
+    content2 = f2.read()
+
+print(difflib.SequenceMatcher(None, content1, content2).ratio())
+print(content1)
+print(content2)
+
+diff = [(i, j) for i, (a, b) in enumerate(zip(content1, content2)) for j, (c, d) in enumerate(zip(content1, content2)) if a != c or b != d]
+
+for i, j in diff:
+    print(f'Difference at position {i} in file1 and position {j} in file2: {content1[i]} vs {content2[j]}')
+print("done")
+
+# 49 50 51 52  [13 10]
+# 53 54 55 56
+
+# 50 51 53 12 10
+# 53 54 55 56
