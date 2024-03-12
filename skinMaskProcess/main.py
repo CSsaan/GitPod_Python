d0b5a1b42ce5c491267aec61b9a5dcb8487ab8f9
CSsaan <1179593516@qq.com>
2024-03-12 14:59:28 +0000

    新增skinMaskProcess

---
+++ skinMaskProcess/main.py
@@ -0,0 +1,163 @@
+import os
+import cv2
+import numpy as np
+
+from skinSeg import *
+
+class VideoProcessor:
+    def __init__(self, video_path):
+        # 检查文件是否存在
+        if not os.path.isfile(video_path):
+            raise FileNotFoundError("视频文件不存在")
+        self.video_path = video_path
+        self.cap = cv2.VideoCapture(video_path)
+    
+    def get_cap_status(self):
+        return self.cap.isOpened()
+
+    def get_frame(self, frame_number=None):
+        if(frame_number != None):
+            self.cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+        ret, frame = self.cap.read()
+        if not ret:
+            raise ValueError("无法读取视频帧")
+        return frame
+
+    def rgb_to_gray(self, rgb_image):
+        gray_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2GRAY)
+        return gray_image
+
+    def binarize_image(self, image, threshold):
+        image = self.rgb_to_gray(image)
+        _, binary_image = cv2.threshold(image, threshold, 255, cv2.THRESH_BINARY)
+        return binary_image
+
+    def filter_green(self, rgb_image):
+        green_mask = rgb_image[:,:,1] > 200
+        red_mask = rgb_image[:,:,2] < 100
+        filtered_mask = np.logical_and(green_mask, red_mask).astype(np.uint8) * 255
+        filtered_image = cv2.bitwise_and(rgb_image, rgb_image, mask=filtered_mask)
+        return filtered_image
+
+    def dilate_image(self, image, kernel_size):
+        kernel = np.ones((kernel_size, kernel_size), np.uint8)
+        dilated_image = cv2.dilate(image, kernel, iterations=1)
+        return dilated_image
+
+    def merge_mask(self, skin_YCrCb, distance, origin_frame, dilated_mask):
+        Tradition = skin_YCrCb[:,:,2]
+        result = np.zeros(Tradition.shape, np.uint8)
+        # 遍历单通道图像的所有像素值
+
+        condition1 = (origin_frame > 0) & (Tradition > 0)
+        condition2 = (origin_frame > 0) & (Tradition == 0)
+        condition3 = (origin_frame == 0) & (Tradition > 0) & (dilated_mask > 0)
+        result[(condition1)] = 255
+        result[(condition2)] = 150
+        result[(condition3)] = distance[condition3]
+        return result
+    
+    def gaussian_blur(self, mergeMask, kernel_size=15):
+        blurred_image = cv2.GaussianBlur(mergeMask, (0, 0), kernel_size)
+        return blurred_image
+
+    def blend_images(self, image1, image2, mask):
+        blended = cv2.seamlessClone(image1, image2, mask, (mask.shape[1]//2, mask.shape[0]//2), cv2.NORMAL_CLONE)
+        return blended
+
+    def save_image(self, image, filename):
+        cv2.imwrite(filename, image)
+    
+    def quite_cap(self):
+        self.cap.release()
+
+
+if __name__ == "__main__":
+    # 进行单帧效果测试
+    USE_SINGLE_TEST = False
+    if USE_SINGLE_TEST:
+        print("Single Test")
+        frame_n = 1
+        # 拿取原视频第 frame_n 帧
+        video_processor0 = VideoProcessor('/workspace/GitPod_Python/skinMaskProcess/dataset/origin.mp4')
+        frame0 = video_processor0.get_frame(frame_n)
+        video_processor0.save_image(frame0, f'/workspace/GitPod_Python/skinMaskProcess/result/origin_{frame_n}.jpg')
+
+        skin_YCrCb, distance = YCrCbSkin(frame0)
+        video_processor0.save_image(distance, f'/workspace/GitPod_Python/skinMaskProcess/result/distance_{frame_n}.jpg')
+        video_processor0.save_image(skin_YCrCb, f'/workspace/GitPod_Python/skinMaskProcess/result/origin_{frame_n}_skin.jpg')
+
+        # 拿取AIMask第 frame_n 帧
+        video_processor = VideoProcessor('/workspace/GitPod_Python/skinMaskProcess/dataset/AImask.avi')
+        frame = video_processor.get_frame(frame_n)
+        video_processor.save_image(frame, f'/workspace/GitPod_Python/skinMaskProcess/result/AImask_{frame_n}.jpg')
+        frame = video_processor.filter_green(frame)
+        # gray_frame = video_processor.rgb_to_gray(frame)
+        binarize_frame = video_processor.binarize_image(frame, threshold=1)
+        video_processor.save_image(binarize_frame, f'/workspace/GitPod_Python/skinMaskProcess/result/AImask_{frame_n}_binarize.jpg')
+
+        # AI结果进行膨胀
+        dilated_mask = video_processor.dilate_image(binarize_frame, kernel_size=80)
+        video_processor.save_image(dilated_mask, f'/workspace/GitPod_Python/skinMaskProcess/result/AImask_{frame_n}_dilated.jpg')
+
+        # 融合 skin_YCrCb + dilated_mask
+        mergeMask = video_processor.merge_mask(skin_YCrCb, distance, binarize_frame, dilated_mask)
+        video_processor.save_image(mergeMask, f'/workspace/GitPod_Python/skinMaskProcess/result/mergeMask_{frame_n}.jpg')
+
+        # 高斯模糊
+        blurred_mask = video_processor.gaussian_blur(mergeMask, kernel_size=21)
+        video_processor.save_image(blurred_mask, f'/workspace/GitPod_Python/skinMaskProcess/result/mergeMaskBlurred_{frame_n}.jpg')
+
+        # LUT调色
+        # 根据mask融合两张图
+        height, width, _ = frame0.shape
+        red_image = np.full((height, width, 3), (0, 0, 255), dtype=np.uint8)
+        result = video_processor.blend_images(red_image, frame0, 0.5)
+        video_processor.save_image(result, f'/workspace/GitPod_Python/skinMaskProcess/result/result_{frame_n}.jpg')
+
+    # 进行所有帧效果测试
+    else:
+        print("Processing all frames...")
+        # 创建一个名为Frame的窗口，并设置窗口大小
+        cv2.namedWindow('Frame', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Frame', 620*2, 1080)  # 设置窗口大小为800x600
+        frame_n = 1 # AI视频帧定位
+
+        video_processor = VideoProcessor('./skinMaskProcess/dataset/AImask.avi') # AI视频加载器
+        video_processor0 = VideoProcessor("./skinMaskProcess/dataset/origin.mp4") # 原视频加载器
+        while video_processor0.get_cap_status():
+            frame0 = video_processor0.get_frame()
+            skin_YCrCb, distance = YCrCbSkin(frame0)
+
+            frameAI = video_processor.get_frame(frame_n)
+            frame_n += 1
+            frame = video_processor.filter_green(frameAI)
+            binarize_frame = video_processor.binarize_image(frame, threshold=1)
+            dilated_mask = video_processor.dilate_image(binarize_frame, kernel_size=121)
+            mergeMask = video_processor.merge_mask(skin_YCrCb, distance, binarize_frame, dilated_mask)
+            mergeMask = video_processor.gaussian_blur(mergeMask, kernel_size=5)
+
+            height, width, _ = frame0.shape
+            red_image = np.full((height, width, 3), (0, 0, 255), dtype=np.uint8)
+            # blended = cv2.addWeighted(frame0, 0.6, red_image, 0.4, 0)
+            # mergeMask由单通道变为三通道
+            # mergeMask = cv2.cvtColor(mergeMask, cv2.COLOR_GRAY2BGR)
+            mergeMask = mergeMask.astype(float) / 255
+            blended = np.zeros_like(frame0)
+            blended[:, :, 0] = frame0[:, :, 0] * (1 - mergeMask) + red_image[:, :, 0] * mergeMask
+            blended[:, :, 1] = frame0[:, :, 1] * (1 - mergeMask) + red_image[:, :, 1] * mergeMask
+            blended[:, :, 2] = frame0[:, :, 2] * (1 - mergeMask) + red_image[:, :, 2] * mergeMask
+
+            result = cv2.hconcat([frameAI, blended])
+            # 显示查看一下
+            cv2.imshow("Frame", skin_YCrCb)
+            key = cv2.waitKey(30)
+            if key == ord('q') or key == 27:
+                print("Exiting...")
+                video_processor0.quite_cap()
+                cv2.destroyAllWindows()
+                break
+
+        # 释放视频对象和关闭窗口
+        video_processor0.quite_cap()
+        cv2.destroyAllWindows()
---
+++ skinMaskProcess/skinSeg.py
@@ -0,0 +1,102 @@
+import cv2
+import numpy as np
+
+def RGBSkin(my_texture):
+    # result = np.zeros_like(my_texture)
+    result = np.copy(my_texture)
+    R = my_texture[:,:,2]
+    G = my_texture[:,:,1]
+    B = my_texture[:,:,0]
+    shift = 0.0 # 控制范围
+    condition1 = (R > 95.0 + shift) & (G > 40.0 + shift) & (B > 20.0 + shift) & ((np.maximum(R, np.maximum(G, B)) - np.minimum(R, np.minimum(G, B)) > 15.0 + shift)) & (np.abs(R - G) > 15.0 + shift) & (R > G) & (R > B)
+    condition2 = (R > 200.0 + shift) & (G > 210.0 + shift) & (B > 170.0 + shift) & (np.abs(R - G) <= 15.0 - shift) & (R > B) & (G > B)
+    # result[condition1 | condition2] =  [255, 255, 255]
+    result[~(condition1 | condition2)] = [0, 0, 0]  # 这里的[0, 0, 51, 255]对应vec4(0.0, 0.0, 0.2, 1.0)
+    R = result[:,:,2]
+    G = result[:,:,1]
+    B = result[:,:,0]
+    condition = (R>240) & (G>210) & (B>234)
+    result[condition] = [0, 0, 0]
+    return result
+
+def YCrCbSkin(my_texture): 
+    # opencv转YCrCb
+    in_img = np.copy(my_texture)
+    my_texture = cv2.cvtColor(my_texture, cv2.COLOR_BGR2RGB)
+    img_YCrCb = cv2.cvtColor(my_texture, cv2.COLOR_RGB2YCrCb)
+    # 判断条件Cr>=133.0 && Cr<=173.0 && Cb>=77.0 && Cb<=127.0
+    shift = 0.0 # 控制范围
+    skin_region = np.where((img_YCrCb[:, :, 1] >= 133.0 + shift) & (img_YCrCb[:, :, 1] <= 173.0 - shift) & (img_YCrCb[:, :, 2] >= 77.0 + shift) & (img_YCrCb[:, :, 2] <= 127.0 - shift))
+    # 满足skin_region条件，则是原图像素，否则是黑色
+    img_skin = np.zeros(my_texture.shape, np.uint8)
+    img_skin[skin_region] = in_img[skin_region]
+    # 计算颜色距离
+    cb_diff = np.abs(img_YCrCb[:, :, 1] - (133.0+173.0)*0.5) # max:153
+    cr_diff = np.abs(img_YCrCb[:, :, 2] - (77.0+127.0)*0.5)  # max:102
+    distance = np.sqrt(cb_diff**2 + cr_diff**2) # max:1154
+    # 根据图像像素最小值与最大值进行归一化
+    # distance = ((distance - distance.min()) / (distance.max() - distance.min()))*255
+    distance = distance / 102.0
+    # opencv将图像转为0-255 int类型
+    distance = cv2.convertScaleAbs(distance, alpha=(255.0))
+    
+    # 黑白取反
+    distance = 220 - distance
+    return img_skin, distance
+
+def HSVSkin(my_texture):
+    
+    img_skin = np.copy(my_texture)
+    my_texture = cv2.cvtColor(my_texture, cv2.COLOR_BGR2RGB)
+    img_hsv = cv2.cvtColor(my_texture, cv2.COLOR_RGB2HSV)
+    # 判断条件 HSV.x>=0.0 && HSV.x<=20.0 && HSV.y>=48.0 && HSV.z>=50.0
+    # skin_region = np.where((img_hsv[:, :, 0] >= 0.0) & (img_hsv[:, :, 0] <= 20.0) & (img_hsv[:, :, 1] >= 48.0) & (img_hsv[:, :, 1] <= 50.0))
+    H = img_hsv[:,:,0]
+    S = img_hsv[:,:,1]
+    V = img_hsv[:,:,2]
+    print("H MAX min:", H.max(), H.min(),"S MAX min:", S.max(), S.min(),"V MAX min:", V.max(), V.min())
+    shift = 2.0 # 控制范围
+    condition = (H>=0.0 + shift) & (H<=20.0 - shift) & (S>=48.0 + shift) & (V>=50.0 + shift)
+    # 满足skin_region条件，则是原图像素，否则是黑色
+    img_skin[~(condition)] = [0, 0, 0]
+    return img_skin
+
+def SkinInsight(imageA, imageB):
+    imageB = imageB.astype('float32')
+    # 将图像B转换为灰度图像
+    grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+    # 将灰度图像转换为单精度浮点型图像
+    normalized_grayB = grayB.astype('float64') / 255.0
+    # 融合图像A和归一化的灰度图像B
+    result = cv2.addWeighted(imageA, 1.0, imageB, normalized_grayB, 0.0)
+    return result
+
+def wrinkles(img, output_folder, mode):
+    '''
+    皮肤光滑度：毛孔、 斑点和皱纹等瑕疵
+    '''
+    if(mode == "GaussianBlur"):
+        img_blur = cv2.GaussianBlur(img, (9, 9), 0) # opencv高斯模糊
+    if(mode == "blur"):
+        img_blur = cv2.blur(img, (5, 5)) # opencv均值模糊
+    if(mode == "medianBlur"):
+        img_blur = cv2.medianBlur(img, 5) # opencv中值模糊
+    if(mode == "bilateralFilter"):
+        img_blur = cv2.bilateralFilter(img, 9, 75, 75) # opencv双边滤波
+    cv2.imwrite('/workspace/GitPod_Python/ImageQualityEvaluation/result/img_blur.jpg', img_blur)
+    diff = cv2.absdiff(img, img_blur) # 原图与模糊图差的可视化
+    cv2.imwrite(output_folder, diff)
+    return diff
+
+if __name__ == "__main__":
+    # 读取图像
+    pngFile = "/workspace/GitPod_Python/ImageQualityEvaluation/dataset/face5.png"
+    image = cv2.imread(pngFile)
+    # 调用RGBSkin函数进行mask获取
+    skin_image = RGBSkin(image)
+    # 显示结果图像
+    cv2.imwrite('/workspace/GitPod_Python/ImageQualityEvaluation/result/face5-l_skinSeg.jpg', skin_image)
+    print('saved png : ' + '/workspace/GitPod_Python/ImageQualityEvaluation/result/face5-l_skinSeg.jpg')
+
+
+    
