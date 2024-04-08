7b1f7c58660645b7254e657a12d89d27c643aef3
CSsaan <57102493+CSsaan@users.noreply.github.com>
2024-04-08 07:26:27 +0000

    add Cmake Lib test

---
+++ test_cmakeLib/src/hi.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "hi.h"
+
+void hi() {
+    std::cout << "hi from MyLib!" << std::endl;
+}
---
+++ test_cmakeLib/use_lib/use_lib.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "hello.h"
+#include "hi.h"
+
+int main()
+{
+    hello();
+
+    hi();
+    
+    return 0;
+}
